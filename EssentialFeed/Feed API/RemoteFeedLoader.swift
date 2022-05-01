d0dd2ca802c853b214d8d2a505a3aa59e0bb9a03
Carlos Kimura <carloskimura@Carloss-MacBook-Pro.local>
2022-05-01 11:08:50 -0300

    Move 'RemoteFeedLoader' components to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Carlos Kimura on 01/05/22.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
+
